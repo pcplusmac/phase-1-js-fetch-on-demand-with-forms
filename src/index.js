6e01d16a0994468edb4588e6902d8437e8de513e
el-sam <samurai.h@yahoo.com>
2025-10-29 10:48:50 +0800

    change title element to innerHtml to add the style

--- src/index.js
+++ src/index.js
@@ -20,8 +20,9 @@ function addToList(book){
     let summaryValue = book.summary
     let title =document.querySelector("#movieDetails h4")
     let p =document.querySelector("#movieDetails p")
-    title.textContent = `we found the book with title: ${titleValue}`
+    title.innerHTML = `we found the book with title: <h3>${titleValue}</h3>`
     p.textContent = `this is the summary of the book: ${summaryValue}`
+    // p.style.color = "red"
 }
 
 document.addEventListener('DOMContentLoaded', init);
